96a9460a81a2e1679ea75e2519cbde7550952131
jfillman <crossfader@gmail.com>
2026-08-08 19:35:03 -0700

    Trivial change to test the full build->test->deploy chain post broker fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.json({
-    message: 'hello from cicd-flow-test-app!!!',
+    message: 'hello from cicd-flow-test-app (full chain test)!!!',
     version: process.env.APP_VERSION || 'dev',
   });
 });
